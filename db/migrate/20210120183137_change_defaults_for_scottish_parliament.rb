880e147af4cfa61d11aecd71c366d34710fe2459
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-01-21 15:59:31 +0000

    db: fix a typo in the Scottish defaults migration

--- db/migrate/20210120183137_change_defaults_for_scottish_parliament.rb
+++ db/migrate/20210120183137_change_defaults_for_scottish_parliament.rb
@@ -12,8 +12,8 @@ class ChangeDefaultsForScottishParliament < ActiveRecord::Migration[5.2]
 
     change_column_default :sites,
                           :email_from_en,
-                          from: %["Petitions: Welsh Parliament" <no-reply@petition.senedd.wales>"],
-                          to:   %["Petitions: Scottish Parliament" <no-reply@petitions.parliament.scot>"]
+                          from: %["Petitions: Welsh Parliament" <no-reply@petition.senedd.wales>],
+                          to:   %["Petitions: Scottish Parliament" <no-reply@petitions.parliament.scot>]
 
     change_column_default :sites, :moderate_url,
                           from: %[https://moderate.petitions.senedd.wales],
@@ -35,7 +35,7 @@ class ChangeDefaultsForScottishParliament < ActiveRecord::Migration[5.2]
 
     change_column_default :sites,
                           :email_from_cy,
-                          from: %["Deisebau: Senedd" <dim-ateb@deisebau.senedd.cymru>"],
+                          from: %["Deisebau: Senedd" <dim-ateb@deisebau.senedd.cymru>],
                           to:   %["Athchuingean: Pàrlamaid na h-Alba" <gun-fhreagairt@athchuingean.parlamaid-alba.scot>]
   end
 end
