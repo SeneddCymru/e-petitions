29ae57ab35575f2347e2384f4d503d3b646e1052
Andrew White <andrew.white@unboxed.co>
2021-02-08 07:43:08 +0000

    Wrap initializer code in to_prepare blocks
    Rails has deprecated the autoloading of code during the boot process
    as it doesn't get reloaded in development so we need to wrap the
    affected code in a to_prepare block which will be executed every time
    the app is reloaded.

--- config/initializers/delayed_web.rb
+++ config/initializers/delayed_web.rb
@@ -1,26 +1,26 @@
-# Tell Delayed::Web that we're using ActiveRecord as our backend.
 Rails.application.config.to_prepare do
+  # Tell Delayed::Web that we're using ActiveRecord as our backend.
   Delayed::Web::Job.backend = 'active_record'
-end
 
-# Authenticate our delayed job web interface
-Delayed::Web::ApplicationController.class_eval do
-  include Authentication, FlashI18n
+  # Authenticate our delayed job web interface
+  Delayed::Web::ApplicationController.class_eval do
+    include Authentication, FlashI18n
 
-  def admin_request?
-    true
+    def admin_request?
+      true
+    end
+
+    protected
+
+    def admin_login_url
+      main_app.admin_login_url
+    end
   end
 
-  protected
-
-  def admin_login_url
-    main_app.admin_login_url
-  end
-end
-
-# The gem still uses update_attributes! which has now been removed.
-class Delayed::Web::ActiveRecordDecorator < SimpleDelegator
-  def queue! now = Time.current
-    update! run_at: now, failed_at: nil, last_error: nil
+  # The gem still uses update_attributes! which has now been removed.
+  class Delayed::Web::ActiveRecordDecorator < SimpleDelegator
+    def queue! now = Time.current
+      update! run_at: now, failed_at: nil, last_error: nil
+    end
   end
 end
