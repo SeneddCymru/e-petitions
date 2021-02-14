b28f8f967320b23f83b398217684a1f89303bfa5
Andrew White <andrew.white@unboxed.co>
2021-02-14 23:16:38 +0000

    Tidy up deployment config
    Make default values reflect reality and the gems now detect forking.

--- config/puma.rb
+++ config/puma.rb
@@ -1,24 +1,17 @@
 if ENV["RAILS_ENV"] == "production"
+  environment ENV.fetch("RAILS_ENV")
+
   pidfile "#{File.expand_path('../../tmp/pids/puma.pid', __FILE__)}"
   bind "unix://#{File.expand_path('../../tmp/sockets/puma.sock', __FILE__)}"
 
-  concurrency = {
-    workers: ENV.fetch("WEB_CONCURRENCY") { 4 }.to_i,
-    min_threads: ENV.fetch("WEB_CONCURRENCY_MIN_THREADS") { 8 }.to_i,
-    max_threads: ENV.fetch("WEB_CONCURRENCY_MAX_THREADS") { 32 }.to_i
-  }
+  num_workers = ENV.fetch("WEB_CONCURRENCY") { 2 }.to_i
+  min_threads = ENV.fetch("WEB_CONCURRENCY_MIN_THREADS") { 4 }.to_i
+  max_threads = ENV.fetch("WEB_CONCURRENCY_MAX_THREADS") { 8 }.to_i
 
-  workers(concurrency[:workers])
-  threads(concurrency[:min_threads], concurrency[:max_threads])
+  workers num_workers
+  threads min_threads, max_threads
+
   preload_app!
 
   plugin :appsignal
-
-  on_worker_boot do
-    # Reset the connection to the cache if we can
-    ::Rails.cache.reset if ::Rails.cache.respond_to?(:reset)
-
-    # Establish a connection to the database
-    ActiveRecord::Base.establish_connection
-  end
 end
