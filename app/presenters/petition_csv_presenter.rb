5b6c2ff5554cb531b068a298bfa257631b523e22
Stéphane Maniaci <stephane.maniaci@unboxedconsulting.com>
2021-03-01 15:49:56 +0000

    csv: export the status not the state

--- app/presenters/petition_csv_presenter.rb
+++ app/presenters/petition_csv_presenter.rb
@@ -28,12 +28,12 @@ class PetitionCSVPresenter
   def self.attributes
     if Site.disable_thresholds_and_debates?
       [
-        :action, :background, :previous_action, :additional_details, :state,
+        :action, :background, :previous_action, :additional_details, :status,
         :creator_name, :creator_email, :signature_count, :rejection_code, :rejection_details
       ]
     else
       [
-        :action, :background, :previous_action, :additional_details, :state,
+        :action, :background, :previous_action, :additional_details, :status,
         :creator_name, :creator_email, :signature_count, :rejection_code, :rejection_details,
         :debate_date, :debate_transcript_url, :debate_video_url, :debate_pack_url, :debate_overview
       ]
