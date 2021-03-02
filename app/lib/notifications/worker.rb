5e5c4b94a292f260bd5a98568c44ce2e772994cd
Andrew White <andrew.white@unboxed.co>
2021-03-02 10:11:09 +0000

    Log shoryuken events to a separate namespace

--- app/lib/notifications/worker.rb
+++ app/lib/notifications/worker.rb
@@ -7,6 +7,8 @@ module Notifications
     shoryuken_options body_parser: Event
 
     def perform(message, event)
+      Appsignal.set_namespace("mailer")
+
       if Notification.process!(event)
         message.delete
       end
