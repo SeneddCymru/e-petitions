98b279e2e8cd0fff847f77ea40cf4c211dc70a48
Andrew White <andrew.white@unboxed.co>
2021-04-02 16:22:40 +0100

    Fix syntax error in migration

--- db/migrate/20210401125401_add_lower_index_to_signatures_email.rb
+++ db/migrate/20210401125401_add_lower_index_to_signatures_email.rb
@@ -1,7 +1,7 @@
 class AddLowerIndexToSignaturesEmail < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
-  def change
+  def up
     unless index_exists?(:index_signatures_on_lower_email)
       execute <<~SQL
         CREATE INDEX CONCURRENTLY index_signatures_on_lower_email
@@ -11,10 +11,9 @@ class AddLowerIndexToSignaturesEmail < ActiveRecord::Migration[6.1]
   end
 
   def down
-    if index_exists(:index_signatures_on_lower_email)
+    if index_exists?(:index_signatures_on_lower_email)
       execute <<~SQL
-        DROP INDEX CONCURRENTLY index_signatures_on_lower_email
-        ON signatures;
+        DROP INDEX CONCURRENTLY index_signatures_on_lower_email;
       SQL
     end
   end
@@ -22,6 +21,6 @@ class AddLowerIndexToSignaturesEmail < ActiveRecord::Migration[6.1]
   private
 
   def index_exists?(name)
-    select_value("SELECT to_regclass('#{name}')")
+    select_value("SELECT to_regclass('#{name}')::text")
   end
 end
