ba5f5a58fd8008acc9c701a2c4094452ae28a12c
Andrew White <andrew.white@unboxed.co>
2026-01-17 14:59:25 +0000

    Add foreign key to constituency region

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2022_03_23_174905) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_17_121742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "intarray"
   enable_extension "pg_catalog.plpgsql"
@@ -566,6 +566,7 @@ ActiveRecord::Schema[8.1].define(version: 2022_03_23_174905) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "constituencies", "regions"
   add_foreign_key "constituency_petition_journals", "petitions", on_delete: :cascade
   add_foreign_key "contacts", "signatures", on_delete: :cascade
   add_foreign_key "country_petition_journals", "petitions", on_delete: :cascade
