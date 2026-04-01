1eb18ae12645a27a9183598e3dbea36543eba36b
Andrew White <andrew.white@unboxed.co>
2026-04-01 08:51:24 +0100

    Update list of party political colours
    Reference:
    https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Politics_of_the_United_Kingdom/Index_of_United_Kingdom_political_parties_meta_attributes

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -29,11 +29,12 @@ RSpec.describe Member, type: :model do
 
   describe "#colour" do
     [
-      ['Welsh Liberal Democrats', '#FDBB30'],
-      ['Welsh Labour and Co-operative Party', '#CC0000'],
-      ['Welsh Labour', '#DC241F'],
-      ['Welsh Conservative Party', '#0087DC'],
-      ['Plaid Cymru', '#008142']
+      ['Plaid Cymru', '#008672'],
+      ['Reform UK', '#12B6CF'],
+      ['Welsh Liberal Democrats', '#FF6400'],
+      ['Welsh Labour and Co-operative Party', '#E4003B'],
+      ['Welsh Labour', '#E4003B'],
+      ['Welsh Conservative Party', '#0087DC']
     ].each do |party, colour|
       context "when the member's party is '#{party}'" do
         subject { described_class.new(party: party) }
